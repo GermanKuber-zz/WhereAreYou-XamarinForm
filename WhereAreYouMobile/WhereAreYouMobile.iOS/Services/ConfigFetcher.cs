5db793c3cea0c728e75ca22ecf07bf4326e842f1
Your Name <you@example.com>
2017-03-20 18:04:25 -0300

    Se utiliza XmlDocument

--- WhereAreYouMobile/WhereAreYouMobile.iOS/Services/ConfigFetcher.cs
+++ WhereAreYouMobile/WhereAreYouMobile.iOS/Services/ConfigFetcher.cs
@@ -12,38 +12,47 @@ using Xamarin.Forms;
 
 namespace WhereAreYouMobile.iOS
 {
-    /// <summary>
-    /// Fetches settings from embedded resources in the Android project.
-    /// </summary>
-    public class ConfigFetcher : IConfigFetcher
-    {
-        #region IConfigFetcher implementation
+	/// <summary>
+	/// Fetches settings from embedded resources in the Android project.
+	/// </summary>
+	public class ConfigFetcher : IConfigFetcher
+	{
+		#region IConfigFetcher implementation
 
-        public async Task<string> GetAsync(ConfigurationsKeyEnum configElementName, bool readFromSensitiveConfig = false)
-        {
-            var fileName = (readFromSensitiveConfig) ? "config-sensitive.xml" : "config.xml";
+		public async Task<string> GetAsync(ConfigurationsKeyEnum configElementName, bool readFromSensitiveConfig = false)
+		{
+			try
+			{
 
-            var type = this.GetType();
-            var resource = type.Namespace + ".Config." + fileName;
-            using (var stream = type.Assembly.GetManifestResourceStream(resource))
-            {
-                if (stream == null)
-                    throw new Exception("ConfigFetcher Error - Verifique el NameSpace");
-                using (var reader = new StreamReader(stream))
-                {
-                    var xmlDocument = new XmlDocument();
-                    xmlDocument.LoadXml(await reader.ReadToEndAsync());
-       
-                    if (xmlDocument.GetElementById("config").GetElementsByTagName(configElementName.ToString()) == null)
-                        throw new Exception("ConfigFetcher Error - No se encuentra la configuración solicitada");
 
-                    return xmlDocument.GetElementById("config").GetElementsByTagName(configElementName.ToString())?.Item(0).Value;
-                }
-            }
-        }
+				var fileName = (readFromSensitiveConfig) ? "config-sensitive.xml" : "config.xml";
 
-        #endregion
-    }
+				var type = this.GetType();
+				var resource = type.Namespace + ".Config." + fileName;
+				using (var stream = type.Assembly.GetManifestResourceStream(resource))
+				{
+					if (stream == null)
+						throw new Exception("ConfigFetcher Error - Verifique el NameSpace");
+					using (var reader = new StreamReader(stream))
+					{
+						var xmlDocument = new XmlDocument();
+						xmlDocument.LoadXml(await reader.ReadToEndAsync());
+
+						if (xmlDocument.GetElementsByTagName(configElementName.ToString())[0] == null)
+							throw new Exception("ConfigFetcher Error - No se encuentra la configuración solicitada");
+
+						return xmlDocument.GetElementsByTagName(configElementName.ToString())[0].InnerText;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("Error al parsear el Json de configuraci¨®n al formato esperado");
+			}
+		}
+
+		#endregion
+	}
 
 
 }
